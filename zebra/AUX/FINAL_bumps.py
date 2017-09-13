c2b838afcb6d520b3842083a99bbe8f6f33464c8
Boyan Naydenov <boyan.naydenov@gmail.com>
2017-09-13 12:26:17 +0200

    bumps-zebras finished

--- zebra/AUX/FINAL_bumps.py
+++ zebra/AUX/FINAL_bumps.py
@@ -6,7 +6,7 @@ import argparse
 from imutils.perspective import four_point_transform
 from imutils import contours
 import imutils
-
+import glob
 import cv2
 import numpy as np
 
@@ -22,34 +22,18 @@ def convert_hsv(image):
 	return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # model images
-pass_es = cv2.imread('./test_images/pass_es_g.png',0)
-pass_dr = cv2.imread('./test_images/pass_dr_g.png',0)
 pass_lev = cv2.imread('./test_images/level_g.png',0)
 
 sift = cv2.xfeatures2d.SIFT_create()
-kp1, des_es = sift.detectAndCompute(pass_es,None)
-kp1, des_dr = sift.detectAndCompute(pass_dr,None)
-kp1, des_lev = sift.detectAndCompute(pass_lev,None)
+kp1, pass_lev = sift.detectAndCompute(pass_lev,None)
 
-def ORB(img2):
-	rst, dest2 = cv2.threshold(img2, 130, 255, cv2.THRESH_BINARY)
+no_pass = []
+for file in glob.glob("/home/bobz/repos/OpenCV-programs/visionhack/zebra/test_images/bad/*.png"):
+	img = cv2.imread(file,0)
+	no_pass.append(img)
+kps, no_pass = sift.detectAndCompute(np.array(no_pass),None)
 
-	# find the keypoints and descriptors with SIFT
-	kp2, des2 = orb.detectAndCompute(dest2,None)
 
-	# create BFMatcher object
-	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-	# Match descriptors.
-	matches = bf.match(des1,des2)
-
-	# Sort them in the order of their distance.
-	matches = sorted(matches, key = lambda x:x.distance)
-
-	print matches[0].distance
-	# Draw first 10 matches.
-	# img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None,flags=2)
-	# plt.imshow(img3),plt.show()
 
 def SIFT(img2):
 	sift = cv2.xfeatures2d.SIFT_create()
@@ -61,21 +45,14 @@ def SIFT(img2):
 	search_params = dict(checks = 50)
 
 	flann = cv2.FlannBasedMatcher(index_params, search_params)
-	matches_es = flann.knnMatch(des_es,des2,k=2)
-	matches_dr = flann.knnMatch(des_dr,des2,k=2)
-	matches_lev = flann.knnMatch(des_lev,des2,k=2)
+	matches_np = flann.knnMatch(no_pass,des2,k=2)
+	matches_lev = flann.knnMatch(pass_lev,des2,k=2)
 	# store all the good matches as per Lowe's ratio test.
-	good_es = []
-	for m,n in matches_es:
+	good_np = []
+	for m,n in matches_np:
 		if m.distance < 0.425*n.distance:
-			good_es.append([m,n])
-	size_es = len(good_es)
-
-	good_dr = []
-	for m,n in matches_dr:
-		if m.distance < 0.425*n.distance:
-			good_dr.append([m,n])
-	size_dr = len(good_dr)
+			good_np.append([m,n])
+	size_np = len(good_np)
 
 	good_lev = []
 	for m,n in matches_lev:
@@ -83,17 +60,14 @@ def SIFT(img2):
 			good_lev.append([m,n])
 	size_lev = len(good_lev)
 
-	if(size_es > size_lev):
-		return (size_es,'z') if size_es > size_dr else (size_dr,'z')
-	elif(size_dr > size_lev):
-		return (size_dr, 'z') if size_dr > size_es else (size_es,'z')
-	elif(size_es == size_lev):
-		return (size_es, 'z') if good_es[0].distance < good_lev[0].distance else (size_lev,'l')
-	elif(size_dr == size_lev):
-		return (size_dr, 'z') if good_dr[0].distance < good_lev[0].distance else (size_lev, 'l')
+	if(size_lev > size_np):
+		return (size_lev,'l')
+	elif(size_np > size_lev):
+		return (0, 'n')
+	elif(size_np == size_lev):
+		return (size_lev, 'l') if good_lev[0].distance < good_np[0].distance else (0,'n')
 	else:
-		return (size_lev, 'l')
-
+		return (0,'n')
 zebra = False
 level = False
 def defineTrafficSign(image):
@@ -144,13 +118,12 @@ def defineTrafficSign(image):
 			try:
 				size, of = SIFT(output);
 				if(int(size) > 1):
-					if (of == 'z'):
-						zebra = True;
-					elif (of == 'l'):
-						level = True
+					if (of == 'l'):
+						cv2.waitKey(-1)
+						level = True;
 			except:
 				pass
-			# cv2.imshow('mask',output)
+			cv2.imshow('mask',output)
 	# extract_triangle(output)
 
 	# threshold the warped image, then apply a series of morphological
@@ -167,18 +140,18 @@ def find(video):
 	global level
 	zebra = False
 	level = False
-	# cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
-	# cv2.resizeWindow('frame', 600,600)
-	# cv2.moveWindow("frame", 0,0);
-	# cv2.namedWindow('mask',cv2.WINDOW_NORMAL)
-	# cv2.resizeWindow('mask', 600,600)
-	# cv2.moveWindow("mask", 620,0);
+	cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('frame', 600,600)
+	cv2.moveWindow("frame", 0,0);
+	cv2.namedWindow('mask',cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('mask', 600,600)
+	cv2.moveWindow("mask", 620,0);
 	cap = cv2.VideoCapture(video)
 	while(cap.isOpened()):
 		(grabbed, frame) = cap.read()
 		if grabbed == False:
 			break
-		# cv2.imshow('frame',frame)
+		cv2.imshow('frame',frame)
 		defineTrafficSign(frame)
 		key = cv2.waitKey(1) & 0xFF
 		if key == ord(u'\u0020'):
@@ -191,17 +164,16 @@ def find(video):
 def bool2num(bool):
 	return 1 if bool else 0
 if __name__ == "__main__":
-	path = '/home/bobz/repos/OpenCV-programs/visionhack/zebra/testset/'
-	# path = '/home/bobz/repos/OpenCV-programs/visionhack/AI/Hack/trainset/'
-	file = open('./results-testset', 'w')
+	path = '/home/bobz/repos/OpenCV-programs/visionhack/zebra/data/'
+	# file = open('./test/results-valid-a', 'w')
 	file_list = [f for f in listdir(path) if isfile(join(path, f)) and f.split('.')[-1] == 'avi']
-	print len(file_list)
+	# print len(file_list)
 	for indx, file_name in enumerate(file_list):
 		find(path + file_name)
 		query_level = str(bool2num(level))
 		query_zebra = str(bool2num(zebra))
 		save = file_name +" "+query_level+query_zebra+"\n"
-		file.write(save)
+		# file.write(save)
 		print "-----------------------------" + str(indx) + " "+file_name
-	file.close()
+	# file.close()
 
